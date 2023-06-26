e22188606d351585a238b7e77f8eaa63be977b06
William Roy <wroy@proton.me>
2023-06-26 14:32:17 -0400

    canvas: create window instance
    This serves primarily as a temporary representation until we can
    transition to a suitable system-independent abstraction.

--- canvas/canvas.cxx
+++ canvas/canvas.cxx
@@ -1,15 +1,56 @@
-#include <iostream>
+#include <Windows.h>
+
+#pragma comment(lib, "Gdi32.lib");
+
+using namespace std;
 
 int
 main (int argc, char* argv[])
 {
-  using namespace std;
+  auto wc (WNDCLASSW {});
+  auto in (GetModuleHandleW (nullptr));
 
-  if (argc < 2)
+  wc.hInstance = in;
+  wc.lpszClassName = L"Sample";
+  wc.lpfnWndProc = [] (const HWND h,
+                       const UINT u,
+                       const WPARAM w,
+                       const LPARAM l)
   {
-    cerr << "error: missing name" << endl;
-    return 1;
-  }
+    switch (u)
+    {
+      case WM_DESTROY:
+      {
+        PostQuitMessage (0);
+        break;
+      }
 
-  cout << "Hello, " << argv[1] << '!' << endl;
+      case WM_PAINT:
+      {
+        PAINTSTRUCT ps;
+
+        FillRect (BeginPaint (h, &ps), &ps.rcPaint,
+                  static_cast <HBRUSH> (GetStockObject (BLACK_BRUSH)));
+
+        EndPaint (h, &ps);
+        break;
+      }
+      default: ;
+    }
+    return DefWindowProcW (h, u, w, l);
+  };
+
+  RegisterClassW (&wc);
+  ShowWindow (CreateWindowExW (0, L"Sample", L"Sample", WS_OVERLAPPEDWINDOW,
+                               CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+                               CW_USEDEFAULT, nullptr, nullptr, in, nullptr), SW_SHOW);
+
+  MSG msg = {};
+  {
+    while (GetMessageW (&msg, nullptr, 0, 0) > 0)
+    {
+      TranslateMessage (&msg);
+      DispatchMessageW (&msg);
+    }
+  }
 }

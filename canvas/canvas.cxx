bdb707295026fbf51993c8c5712928f1ef02592c
William Roy <wroy@proton.me>
2023-06-29 18:01:46 -0400

    canvas: create frame context and draw rectangle
    First prototype rendering. Naturally, this is an ongoing experimental
    phase as Vector graphics APIs have very, very few resources available.

--- canvas/canvas.cxx
+++ canvas/canvas.cxx
@@ -1,8 +1,101 @@
 #include <Windows.h>
 
-#pragma comment(lib, "Gdi32.lib");
+#include <skia/core/SkCanvas.h>
+#include <skia/core/SkColorSpace.h>
+#include <skia/core/SkPaint.h>
+#include <skia/core/SkRect.h>
+#include <skia/core/SkSurface.h>
+#include <skia/gpu/GrDirectContext.h>
+
+#include <GL/gl.h>
 
 using namespace std;
+
+namespace canvas
+{
+  namespace
+  {
+    struct pixel
+    {
+      struct
+      {
+        int s;
+        PIXELFORMATDESCRIPTOR descriptor;
+
+        auto&
+        operator= (const int v)
+        {
+          return s = v, *this;
+        }
+
+        operator int () const
+        {
+          return s;
+        }
+      } format;
+    };
+
+    struct context
+    {
+      unique_ptr<HGLRC> gl;
+      unique_ptr<sk_sp<GrDirectContext>> skia;
+    };
+
+    void
+    draw_frame (const HDC& h)
+    {
+      static auto [format] (pixel {});
+      static auto ctx (context {});
+      static auto flag (once_flag {});
+
+      auto create_context = [&]
+      {
+        format.descriptor.nSize = sizeof format.descriptor;
+        format.descriptor.nVersion = 1;
+        format.descriptor.dwFlags = PFD_DRAW_TO_WINDOW
+                                  | PFD_SUPPORT_OPENGL
+                                  | PFD_DEPTH_DONTCARE;
+        format.descriptor.iPixelType = PFD_TYPE_RGBA;
+        format.descriptor.cColorBits = 24;
+        format.descriptor.cAlphaBits = 8;
+        format.descriptor.iLayerType = PFD_MAIN_PLANE;
+        format = ChoosePixelFormat (h, &format.descriptor);
+        DescribePixelFormat (h, format, sizeof (PIXELFORMATDESCRIPTOR),
+                             &format.descriptor);
+        SetPixelFormat (h, format, &format.descriptor);
+        ctx.gl = make_unique <HGLRC> (wglCreateContext (h));
+        wglMakeCurrent (h, *ctx.gl);
+        ctx.skia = make_unique <sk_sp <GrDirectContext>> (
+          GrDirectContext::MakeGL ());
+      };
+
+      call_once (flag, create_context);
+      SetPixelFormat (h, format, &format.descriptor);
+
+      const auto props = SkSurfaceProps {};
+      const auto surf = SkSurface::MakeFromBackendRenderTarget (
+        ctx.skia->get (),
+        GrBackendRenderTarget (500, 500, 1, 0,
+                               GrGLFramebufferInfo (0, GL_RGBA8)),
+        kBottomLeft_GrSurfaceOrigin, kRGBA_8888_SkColorType,
+        SkColorSpace::MakeSRGB (), &props);
+      {
+        auto  pa = SkPaint {};
+        auto& ca = *surf->getCanvas ();
+
+        ca.save ();
+        ca.clear (SK_ColorBLACK);
+        pa.setColor (SK_ColorBLUE);
+        pa.setStyle (SkPaint::kFill_Style);
+        ca.drawRect (SkRect::MakeXYWH (100, 100, 200, 200), pa);
+        ca.restore ();
+
+        ctx.skia->get ()->flushAndSubmit (true);
+        SwapBuffers (h);
+      }
+    }
+  }
+}
 
 int
 main (int argc, char* argv[])
@@ -28,14 +121,21 @@ main (int argc, char* argv[])
       case WM_PAINT:
       {
         PAINTSTRUCT ps;
-
-        FillRect (BeginPaint (h, &ps), &ps.rcPaint,
-                  static_cast <HBRUSH> (GetStockObject (BLACK_BRUSH)));
-
+        canvas::draw_frame (BeginPaint (h, &ps));
         EndPaint (h, &ps);
         break;
       }
-      default: ;
+
+      case WM_SETCURSOR:
+      {
+        SetCursor (LoadCursor (nullptr, IDC_ARROW));
+        break;
+      }
+
+      default:
+      {
+        break;
+      }
     }
     return DefWindowProcW (h, u, w, l);
   };
@@ -45,12 +145,12 @@ main (int argc, char* argv[])
                                CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
                                CW_USEDEFAULT, nullptr, nullptr, in, nullptr), SW_SHOW);
 
-  MSG msg = {};
+  auto m (MSG {});
   {
-    while (GetMessageW (&msg, nullptr, 0, 0) > 0)
+    while (GetMessageW (&m, nullptr, 0, 0) > 0)
     {
-      TranslateMessage (&msg);
-      DispatchMessageW (&msg);
+      TranslateMessage (&m);
+      DispatchMessageW (&m);
     }
   }
 }
